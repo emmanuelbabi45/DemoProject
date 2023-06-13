1762a058717f45b236ed2721afc113bd84e6c687
emmanuel <govindapalliemmanuel045@gmail.com>
2023-06-13 13:57:03 +0530

    secondcommit

--- DemoApp1/views.py
+++ DemoApp1/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def f1(req):
-    return HttpResponse("<h1> Hello from demo app1 using f1()</h1><hr/>");
+    return HttpResponse("<h1> ****Hello from demo app1 using f1()****</h1><hr/>");
 def f2(req):
     return HttpResponse("<h2> hello from demo app2 using f2()</h2><hr />");
